a8cebb7a892a7afc24427cefc79b8fa803ffa95b
Hiromu Ochiai <otiai10@gmail.com>
2018-12-20 23:36:21 +0900

    Slackへのデプロイアナウンスをちゃんと内容言うようにした

--- scripts/slack.sh
+++ scripts/slack.sh
@@ -1,9 +1,13 @@
+#!/bin/sh
 
 DATE=`date`
+LAST=`git log --author=ayanel-ci -1 --pretty='%H'`
+# PREFIX='*'
+PREFIX='・'
+COMMITS=`git log --pretty="%s" "${LAST}..HEAD" | sed -e "s/^/${PREFIX} /"`
 
 curl -XPOST \
     -H "Content-Type: application/json; charset=utf-8" \
     -H "Authorization: Bearer ${SLACK_BOT_TOKEN}" \
-    -d "{'text': '${DATE}\nホームページ更新デプロイしたから見て！\nhttps://yukemuli.dance', 'channel':'homepage'}" \
+    -d "{'mrkdwn': true, 'text': '_${DATE}_\nホームページ更新デプロイしたから見て！\n\n${COMMITS}\n\nhttps://yukemuli.dance', 'channel':'${SLACK_CHANNEL}'}" \
     "https://slack.com/api/chat.postMessage"
-
